003f4ec6a9fc8ae27e5d22872ff4e3e5cfae3d56
DiguyaDeveloper <diegocecconcwb@outlook.com>
2023-10-05 08:46:09 -0300

    chore: 👷 remove invalid data

--- sonar-project.js
+++ sonar-project.js
@@ -4,7 +4,7 @@ sonarqubeScanner({
     options: {
         'sonar.sources': '.',
         'sonar.inclusions': '**/src/**', // Entry point of your code
-        'sonar.login': 'sqp_9e05b05f7909adc6fa2e7ba190c09c18a47f4d46',
+        'sonar.login': 'sqp_9e05b05f7ABCD123456ABC190c09c18a47f4d46',
         'sonar.projectKey': 'ceccoff-sonarqube-example'
     }
 }, () => { });
